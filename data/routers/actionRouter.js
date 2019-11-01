671737f76db66e7528bb1fec91665c1514cc45cf
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-01 08:59:11 +0000

    get action by ID working

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -18,4 +18,29 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", validateActionId, (req, res, next) => {
+  const action = req.action;
+  res.status(200).json(action);
+});
+
+function validateActionId(req, res, next) {
+  const { id } = req.params;
+  Actions.get(id)
+    .then(action => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        res.status(400).json({
+          message: "invalid id"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+}
+
 module.exports = router;
