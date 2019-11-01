c173c47d10350fa0d1e42d2828a6edf5e04c8c38
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-01 09:35:29 +0000

    update working for actions and projects

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -54,6 +54,24 @@ router.delete("/:id", validateActionId, (req, res) => {
     });
 });
 
+router.put("/:id", validateActionId, (req, res) => {
+  const { id } = req.params;
+  const updatedAction = req.body;
+  Actions.update(id, updatedAction)
+    .then(action => {
+      res.status(200).json({
+        message: "Action successfully updated",
+        action
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error updating the action",
+        error: err.message
+      });
+    });
+});
+
 function validateActionId(req, res, next) {
   const { id } = req.params;
   Actions.get(id)
--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -4,7 +4,7 @@ const Projects = require("../helpers/projectModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
- Projects.get()
+  Projects.get()
     .then(projects => {
       res.status(200).json({
         projects,
@@ -19,61 +19,77 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", validateProjectId, (req, res) => {
-    const project = req.project;
-    res.status(200).json(project);
-  });
+  const project = req.project;
+  res.status(200).json(project);
+});
 
-  router.post("/", (req, res) => {
-    const newProject = req.body;
-    Projects.insert(newProject)
-      .then(project => {
-        res.status(200).json({
-          project,
-          message: "Project successfully added"
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: "Something went wrong when adding the project",
-          error: err.message
-        });
+router.post("/", (req, res) => {
+  const newProject = req.body;
+  Projects.insert(newProject)
+    .then(project => {
+      res.status(200).json({
+        project,
+        message: "Project successfully added"
       });
-  });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Something went wrong when adding the project",
+        error: err.message
+      });
+    });
+});
 
-  router.delete("/:id", validateProjectId, (req, res) => {
-    Projects.remove(req.project.id)
-      .then(() => {
-        res.status(200).json({
-          message: "Project successfully deleted"
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: `Error removing the project: ${err.message}`
-        });
+router.delete("/:id", validateProjectId, (req, res) => {
+  Projects.remove(req.project.id)
+    .then(() => {
+      res.status(200).json({
+        message: "Project successfully deleted"
       });
-  });
-  
-  function validateProjectId(req, res, next) {
-    const { id } = req.params;
-    Projects.get(id)
-      .then(project => {
-        if (project) {
-          req.project = project;
-          next();
-        } else {
-          res.status(400).json({
-            message: "invalid project id"
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: err.message
-        });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Error removing the project: ${err.message}`
       });
-  }
-  
+    });
+});
+
+router.put("/:id", validateProjectId, (req, res) => {
+  const { id } = req.params;
+  const updatedProject = req.body;
+  Projects.update(id, updatedProject)
+    .then(project => {
+      res.status(200).json({
+        message: "Project successfully updated",
+        project
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error updating the project",
+        error: err.message
+      });
+    });
+});
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+  Projects.get(id)
+    .then(project => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        res.status(400).json({
+          message: "invalid project id"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+}
 
 module.exports = router;
-
